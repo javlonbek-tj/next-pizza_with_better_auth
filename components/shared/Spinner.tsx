7da6f9c8f55c9f472371a1c19f682d61d98dcdc8
tjdeveloper <tjmr1996@gmail.com>
2026-03-03 06:30:54 +0500

    feat: add spinner

--- components/shared/Spinner.tsx
+++ components/shared/Spinner.tsx
@@ -1,15 +1,36 @@
 'use client';
 
-import { cn } from '@/lib';
-
 interface Props {
+  size?: 'sm' | 'md' | 'lg';
   className?: string;
+  showOverlay?: boolean;
 }
 
-export const Spinner = ({ className }: Props) => {
+const sizeMap = {
+  sm: 16,
+  md: 32,
+  lg: 48,
+};
+
+export function Spinner({
+  size = 'md',
+  className = '',
+  showOverlay = true,
+}: Props) {
+  const spinnerElement = (
+    <div
+      className={`border-primary border-b-2 rounded-full animate-spin ${className}`}
+      style={{ width: sizeMap[size], height: sizeMap[size] }}
+    ></div>
+  );
+
+  if (!showOverlay) {
+    return spinnerElement;
+  }
+
   return (
-    <div className={cn('flex justify-center', className)}>
-      <div className='w-7 h-7 border-[3px] border-secondary border-t-primary rounded-full animate-spin' />
+    <div className="z-(--z-overlay) absolute inset-0 flex justify-center items-center bg-white/10 dark:bg-gray-800/10 backdrop-blur-[1px]">
+      {spinnerElement}
     </div>
   );
-};
+}
